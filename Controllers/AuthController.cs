466ab3c14b9a9902032a5e5acc6ca27df405514d
Tobechukwu David Uka <ukadavid7@gmail.com>
2023-09-11 13:22:58 +0100

    Auth, Signup, Login, model done

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PetProject.Model.DTO;
+using PetProject.Service.Interface;
+using System.Web;
+
+namespace PetProject.Controllers
+{
+    [Route("api/user/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+        public AuthController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(SignUp signUp)
+        {
+            var registerUser = await _accountService.RegisterUser(signUp, "USER");
+            if (registerUser.StatusCode == 200)
+            {
+                return Ok(registerUser);
+            }
+            else if (registerUser.StatusCode == 404)
+            {
+                return NotFound(registerUser);
+            }
+            else
+            {
+                return BadRequest(registerUser);
+            }
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(SignInModel signIn)
+        {
+            var loginUser = await _accountService.LoginUser(signIn);
+            if (loginUser.StatusCode == 200)
+            {
+                return Ok(loginUser);
+            }
+            else if (loginUser.StatusCode == 404)
+            {
+                return NotFound(loginUser);
+            }
+            else
+            {
+                return BadRequest(loginUser);
+            }
+        }
+        [HttpPost("forgot_password")]
+        public async Task<IActionResult> ForgotPassword(string email)
+        {
+            var result = await _accountService.ForgotPassword(email);
+            if (result.StatusCode == 200)
+            {
+                return Ok(result);
+            }
+            else if (result.StatusCode == 404)
+            {
+                return NotFound(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+        [HttpPost("reset_password")]
+        public async Task<IActionResult> ResetPassword(ResetPassword resetPassword)
+        {
+            var result = await _accountService.ResetUserPassword(resetPassword);
+            if (result.StatusCode == 200)
+            {
+                return Ok(result);
+            }
+            else if (result.StatusCode == 404)
+            {
+                return NotFound(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+        [HttpPost("confirm-email/{email}")]
+        public async Task<IActionResult> ConfirmEmail (ConfirmEmailTokenDto token,string email)
+        {
+            
+            var result = await _accountService.ConfirmEmailAsync(token.token, email);
+            if (result.StatusCode == 200)
+            {
+                return Ok(result);
+            }
+            else if (result.StatusCode == 404)
+            {
+                return NotFound(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PetProject.Data.Context;
+using PetProject.Data.Repository.Implementation;
+using PetProject.Data.Repository.Interface;
+using PetProject.Model.Entities;
+using PetProject.Service.Implementation;
+using PetProject.Service.Interface;
+using Microsoft.AspNetCore.Cors;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<PetProjectDbContext>().AddDefaultTokenProviders();
+builder.Services.AddDbContext<PetProjectDbContext>(dbContextOptions => dbContextOptions.UseNpgsql(builder.Configuration["ConnectionStrings:DefaultConnection"]));
+builder.Services.AddScoped<IAccountRepo, AccountRepo>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<IGenerateJwt, GenerateJwt>();
+builder.Services.AddScoped<IEmailServices, EmailService>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAllOrigins");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
